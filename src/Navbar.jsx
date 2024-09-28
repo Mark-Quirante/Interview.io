8aa7084e883b07ace682ffdc73b46ce47d5b7864
Mark Quirante <quir0054@algonquinlive.com>
2024-09-28 16:50:42 -0400

    Added an image for Navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,13 @@
+import video from "./assets/InterviewLogo_optimized.gif";
+
 function Navbar() {
 	return (
-		<nav className="bg-green-500">
-			<img></img>
+		<nav className="bg-green-500 p-10">
+			<img
+				src={video}
+				alt="Interview.io Logo"
+				style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+			></img>
 			<p>Land that job!</p>
 			<p>About Us</p>
 		</nav>
