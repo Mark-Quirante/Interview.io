2a507351f5903a5da0cebae7b3c40d336e020891
Mark Quirante <quir0054@algonquinlive.com>
2024-09-28 15:34:32 -0400

    Added useState on Job component input fields

--- src/Job.jsx
+++ src/Job.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 function Job() {
+	const [jobTitle, setJobTitle] = useState("");
+	const [companyName, setCompanyName] = useState("");
+	const [jobDescription, setJobDescription] = useState("");
+
 	return (
 		<div>
 			<div>
@@ -17,19 +22,31 @@ function Job() {
 					<tr>
 						<td>Job Title:</td>
 						<td>
-							<input className="bg-green-500"></input>
+							<input
+								className="bg-green-500"
+								value={jobTitle}
+								onChange={(e) => setJobTitle(e.target.value)}
+							></input>
 						</td>
 					</tr>
 					<tr>
 						<td>Company Name:</td>
 						<td>
-							<input className="bg-green-500"></input>
+							<input
+								className="bg-green-500"
+								value={companyName}
+								onChange={(e) => setCompanyName(e.target.value)}
+							></input>
 						</td>
 					</tr>
 					<tr>
 						<td>Job Description:</td>
 						<td>
-							<input className="bg-green-500"></input>
+							<input
+								className="bg-green-500"
+								value={jobDescription}
+								onChange={(e) => setJobDescription(e.target.value)}
+							></input>
 						</td>
 					</tr>
 				</table>
