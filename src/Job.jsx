dee0eff83e34b14d7c3a9b257f76ae83374cdce5
Mark Quirante <quir0054@algonquinlive.com>
2024-09-28 13:44:03 -0400

    Added two new components.
    Navbar and Job

---
+++ src/Job.jsx
@@ -0,0 +1,31 @@
+function Job() {
+	return (
+		<div>
+			<table>
+				<tr>
+					<thead>Tell me</thead>
+				</tr>
+				<tr>
+					<td>Job Title:</td>
+					<td>
+						<input></input>
+					</td>
+				</tr>
+				<tr>
+					<td>Company Name:</td>
+					<td>
+						<input></input>
+					</td>
+				</tr>
+				<tr>
+					<td>Job Description:</td>
+					<td>
+						<input></input>
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
+}
+
+export default Job;
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+	return (
+		<nav>
+			<img></img>
+			<p>Land that job!</p>
+			<p>About Us</p>
+			<p>Developer Team</p>
+		</nav>
+	);
+}
+export default Navbar;
