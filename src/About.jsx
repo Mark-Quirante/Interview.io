a001eddecdf29b40e5fa08eb013ba0a8036ab9c1
Mark Quirante <quir0054@algonquinlive.com>
2024-09-29 10:08:46 -0400

    Refactord design of about component

--- src/About.jsx
+++ src/About.jsx
@@ -6,31 +6,31 @@ import imageMark from "./assets/Mark.jpg";
 function About() {
 	return (
 		<div>
-			<ul className="flex flex-row text-center">
+			<ul className="flex justify-between flex-row text-center gap-x-8">
 				<li>
 					<img
-						className="object-cover h-96 w-96 rounded-full"
+						className="object-cover h-75 w-96 rounded-full"
 						src={imageAJ}
 					></img>
 					<p className="text-2xl">Augustus Jay Del Rosario</p>
 				</li>
 				<li>
 					<img
-						className="object-cover h-96 w-96 rounded-full"
+						className="object-cover h-75 w-96 rounded-full"
 						src={imageAngel}
 					></img>
 					<p className="text-2xl">Angelica Mapeso</p>
 				</li>
 				<li>
 					<img
-						className="object-cover h-96 w-96 rounded-full"
+						className="object-cover h-44 w-96 rounded-full"
 						src={imageMonica}
 					></img>
 					<p className="text-2xl">Monica Mobashera</p>
 				</li>
 				<li>
 					<img
-						className="object-cover h-96 w-96 rounded-full"
+						className="object-cover h-75 w-96 rounded-full"
 						src={imageMark}
 					></img>
 					<p className="text-2xl">Mark Quirante</p>
